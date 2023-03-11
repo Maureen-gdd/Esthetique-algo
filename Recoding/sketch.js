b3d1747ef0ce6c8344d386683e5d153bc272b155
Maureen-gdd <maureen.grandidier@gmail.com>
2023-03-11 15:52:52 +0100

    Creation of the lines

--- Recoding/sketch.js
+++ Recoding/sketch.js
@@ -1,10 +1,18 @@
-let xoff = 0.0;
+let cnv;
+
+function centerCanvas() 
+{
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+  cnv.position(x, y);
+}
 
 function setup() 
 {
   // width, height
-  createCanvas(700, 700);
-  background(255, 204, 0);
+  cnv = createCanvas(windowWidth*0.8, windowHeight*0.9);
+  centerCanvas();
+  background(0, 0, 0);
 }
 
 function draw() 
@@ -12,8 +20,9 @@ function draw()
   let y = 100;
   for(let i = 0; i < 80; i++)
   {
-    line(width/10, y, 9*width/10, y);
-    y+=5;
+    stroke(255,255,255);
+    line(width*0.35, y, width - width*0.35, y);
+    y+=6;
   }
   /*xoff = xoff + 0.01;
   let n = noise(xoff) * width;
@@ -21,4 +30,9 @@ function draw()
   line(0, n, width, n);
   describe(`vertical line moves left to right with updating
     noise values.`);*/
+}
+
+function windowResized() 
+{
+  centerCanvas();
 }
